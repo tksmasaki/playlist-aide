85067291b235e2311844d8311d0c1a43e5996ac6
tksmasaki <tk.masaki70@gmail.com>
2023-04-17 22:24:44 +0900

    feat: install daisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,9 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  daisyui: {
+    // TODO: darkTheme の設定は後で決める
+    themes: ['cmyk'],
+  },
 };
