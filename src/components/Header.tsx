6c139ec1a0d7f52562a192fd8becb09f6db1303f
tksmasaki <tk.masaki70@gmail.com>
2023-04-29 14:22:03 +0900

    feat: add Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="py-4 shadow">
+      <div className="container mx-auto flex justify-center items-center">
+        <h1 className="text-2xl font-bold">Playlist Aide</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,12 @@
+import Header from '@/components/Header';
 import LoginBtn from '@/components/login-btn';
-import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Playlist Manager</title>
-        <meta
-          name="description"
-          content="Application to create and edit playlists on Spotify easily"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/* TODO: set icon */}
-        {/* <link rel="icon" href="" /> */}
-      </Head>
+      <Header />
       <main>
-        <h1 className="text-3xl font-bold">Playlist Manager</h1>
+        <h1 className="text-3xl font-bold">Playlist Aide</h1>
         <LoginBtn />
       </main>
     </>
