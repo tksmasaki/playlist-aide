a88bf9eabdc28c240883f7909d6cec22f787a7bc
tksmasaki <tk.masaki70@gmail.com>
2023-04-30 19:30:55 +0900

    feat: Add a link to the home page in the title.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,12 @@
+import Link from 'next/link';
+
 const Header = () => {
   return (
     <header className="py-4">
       <div className="container mx-auto flex justify-between items-center px-2">
-        <h1 className="text-2xl font-bold">Playlist Aide</h1>
+        <Link href="/">
+          <h1 className="text-2xl font-bold">Playlist Aide</h1>
+        </Link>
         <div>
           <span className="font-bold">🚧 開発中 🚧</span>
         </div>
