57f3abf62382c51cbb6e39fab316e51cc9e8f51e
tksmasaki <tk.masaki70@gmail.com>
2023-07-23 21:55:03 +0900

    fix: add TmpProductionPage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import Layout from '@/components/Layout';
 import { AppProvider } from '@/context/AppProvider';
 import '@/styles/globals.css';
-import { SessionProvider } from 'next-auth/react';
+import { SessionProvider, signIn, signOut, useSession } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 
 export default function App({
@@ -15,10 +15,40 @@ export default function App({
           {process.env.NODE_ENV === 'development' ? (
             <Component {...pageProps} />
           ) : (
-            <p className="text-center">This app is under development.</p>
+            <TmpProductionPage />
           )}
         </Layout>
       </AppProvider>
     </SessionProvider>
   );
 }
+
+const TmpProductionPage = () => {
+  const { data: session } = useSession();
+
+  if (session === undefined) {
+    return <h2 className="text-center">...</h2>;
+  }
+
+  return (
+    <>
+      <p className="text-center">This app is under development.</p>
+      {session ? (
+        <div className="flex justify-center">
+          <button
+            className="btn-primary btn-wide btn"
+            onClick={() => signOut()}
+          >
+            ログアウト
+          </button>
+        </div>
+      ) : (
+        <div className="flex justify-center">
+          <button className="btn-primary btn" onClick={() => signIn('spotify')}>
+            Spotify でログイン
+          </button>
+        </div>
+      )}
+    </>
+  );
+};
